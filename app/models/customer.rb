055bd44a268ba530e7361662f001d666a4edbc78
Amira de Moya <demoyaamira@gmail.com>
2022-12-23 09:28:09 -0500

    added end on Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,3 +6,5 @@ class Customer < ApplicationRecord
     validates_presence_of :name, :phone, :address, :email, :age
     validates :age, numericality: { greater_than: 17 }
     validates :phone, length: { minimum: 10, maximum: 12 }
+
+end
